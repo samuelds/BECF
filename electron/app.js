0fba2bf04f26fe77bb243e3b0bf85857f484eacb
samuelds <samuelvds.pro@gmail.com>
2016-08-12 14:02:44 +0200

    Add init, check and listeners for client and server

--- electron/app.js
+++ electron/app.js
@@ -1,7 +1,19 @@
-// Server
-console.log('Initializing socket server...')
-require('./server')
 
-// Client
-console.log('Initializing socket server...')
-require('./client')
+const server = require('./server')
+const client = require('./client')
+
+// Init
+client.init()
+server.init()
+
+// check
+client.check()
+server.check()
+if (client.shouldQuit && server.shouldQuit) {
+  client.app.quit();
+  return;
+}
+
+// Listeners
+client.listeners()
+server.listeners()
